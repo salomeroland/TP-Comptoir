c54ef159e9c3e16def5da58cde533e3d6a211149
Rémi Bastide <Remi.Bastide@gmail.com>
2020-05-26 11:16:59 +0200

    Changed schema

--- src/main/java/comptoirs/dao/ProduitRepository.java
+++ src/main/java/comptoirs/dao/ProduitRepository.java
@@ -1,12 +1,10 @@
 package comptoirs.dao;
 
 import java.util.List;
-import javax.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import comptoirs.entity.Produit;
-import comptoirs.dto.StatsResult;
 import comptoirs.dto.UnitesParProduit;
 
 // Cette interface sera auto-implémentée par Spring
@@ -14,25 +12,25 @@ import comptoirs.dto.UnitesParProduit;
 public interface ProduitRepository extends JpaRepository<Produit, Integer> {
 	/**
 	 * Calcule le nombre d'unités vendues pour chaque produit d'une catégorie donnée.
-	 * Utilise le DTO 'StatsResult' pour représenter les résultats de la requête
+	 * pas d'utilisation de DTO
 	 * @param codeCategorie la catégorie à traiter
 	 * @return le nombre d'unités vendus pour chaque produit, 
-	 *		sous la forme d'une liste de StatsResult(nom du produit, quantité vendue)
-	 */
-	@Query("SELECT new comptoirs.dto.StatsResult( p.nom, SUM(li.quantite) ) "
+	 *		sous la forme d'une liste de DTO UnitesParProduit
+	 */	
+	@Query("SELECT p.nom as nom, SUM(li.quantite) AS unites "
 		+ "FROM Categorie c "
 		+ "JOIN c.produitList p "
 		+ "JOIN p.ligneList li "
 		+ "WHERE c.code = :codeCategorie "
 		+ "GROUP BY p.nom ")
-	public List<StatsResult> produitsVendusPour(Integer codeCategorie);
+	public List<UnitesParProduit> produitsVendusPour(Integer codeCategorie);
 	
 	/**
 	 * Calcule le nombre d'unités vendues pour chaque produit d'une catégorie donnée.
 	 * pas d'utilisation de DTO
 	 * @param codeCategorie la catégorie à traiter
 	 * @return le nombre d'unités vendus pour chaque produit, 
-	 *		sous la forme d'une liste de valeurs non typées
+	 *	   sous la forme d'une liste de tableaux de valeurs non typées
 	 */	
 	@Query("SELECT p.nom, SUM(li.quantite) "
 		+ "FROM Categorie c "
@@ -42,33 +40,4 @@ public interface ProduitRepository extends JpaRepository<Produit, Integer> {
 		+ "GROUP BY p.nom ")
 	public List produitsVendusPourV2(Integer codeCategorie);
 
-	/**
-	 * Calcule le nombre d'unités vendues pour chaque produit d'une catégorie donnée.
-	 * pas d'utilisation de DTO
-	 * @param codeCategorie la catégorie à traiter
-	 * @return le nombre d'unités vendus pour chaque produit, 
-	 *		sous la forme d'une liste de valeurs non typées
-	 */	
-	@Query("SELECT p.nom as nom, SUM(li.quantite) AS unites "
-		+ "FROM Categorie c "
-		+ "JOIN c.produitList p "
-		+ "JOIN p.ligneList li "
-		+ "WHERE c.code = :codeCategorie "
-		+ "GROUP BY p.nom ")
-	public List<UnitesParProduit> produitsVendusPourV3(Integer codeCategorie);
-
-	/**
-	 * Calcule le nombre d'unités vendues pour chaque produit d'une catégorie donnée.
-	 * pas d'utilisation de DTO
-	 * @param codeCategorie la catégorie à traiter
-	 * @return le nombre d'unités vendus pour chaque produit, 
-	 *		sous la forme d'une liste de valeurs non typées
-	 */	
-	@Query("SELECT p.nom as nom, SUM(li.quantite) AS unites "
-		+ "FROM Categorie c "
-		+ "JOIN c.produitList p "
-		+ "JOIN p.ligneList li "
-		+ "WHERE c.code = :codeCategorie "
-		+ "GROUP BY p.nom ")
-	public List<Tuple> produitsVendusPourV4(Integer codeCategorie);		
 }
--- src/test/java/comptoirs/ProduitRepositoryTest.java
+++ src/test/java/comptoirs/ProduitRepositoryTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.jdbc.Sql;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import comptoirs.dao.*;
-import comptoirs.dto.StatsResult;
 import comptoirs.dto.UnitesParProduit;
 import java.io.IOException;
 import java.util.List;
@@ -34,45 +33,13 @@ class ProduitRepositoryTest {
 		logger.debug("Calcul des statistiques");
 		int categorieAvecProduit = 1;
 		int categorieSansProduit = 2;
-		List<StatsResult> results  = daoProduit.produitsVendusPour(categorieSansProduit);
+		List<UnitesParProduit> results  = daoProduit.produitsVendusPour(categorieSansProduit);
 		assertEquals(0, results.size(),   
 			"La catégorie 2 n'a pas de produit dans le jeu de test");
 		results = daoProduit.produitsVendusPour(categorieAvecProduit);
 		logger.debug("Résultats de la requête: "+ mapper.writeValueAsString(results));
 		assertEquals(2, results.size(),   
 			"La catégorie 1 a deux produits différents vendus dans le jeu de test");
-		assertEquals(35, results.get(1).unites, "On a vendu 35 unités du produit 'Chang' dans le jeu de test");
-	}
-	
-	@Test
-	@Sql("small_data.sql")		
-	void calculCorrectDesStatistiquesAvecProjection() throws IOException {
-		logger.debug("Calcul des statistiques avec projection");
-		int categorieAvecProduit = 1;
-		int categorieSansProduit = 2;
-		List<UnitesParProduit> results  = daoProduit.produitsVendusPourV3(categorieSansProduit);
-		assertEquals(0, results.size(),   
-			"La catégorie 2 n'a pas de produit dans le jeu de test");
-		results = daoProduit.produitsVendusPourV3(categorieAvecProduit);
-		logger.debug("Résultats de la requête avec projection: "+ mapper.writeValueAsString(results));
-		assertEquals(2, results.size(),   
-			"La catégorie 1 a deux produits différents vendus dans le jeu de test");
 		assertEquals(35, results.get(1).getUnites(), "On a vendu 35 unités du produit 'Chang' dans le jeu de test");
 	}
-
-	@Test
-	@Sql("small_data.sql")		
-	void calculCorrectDesStatistiquesAvecTuple() throws IOException {
-		logger.debug("Calcul des statistiques avec tuple");
-		int categorieAvecProduit = 1;
-		int categorieSansProduit = 2;
-		List<Tuple> results  = daoProduit.produitsVendusPourV4(categorieSansProduit);
-		assertEquals(0, results.size(),   
-			"La catégorie 2 n'a pas de produit dans le jeu de test");
-		results = daoProduit.produitsVendusPourV4(categorieAvecProduit);
-		logger.debug("Résultats de la requête avec tuple: "+ mapper.writeValueAsString(results));
-		assertEquals(2, results.size(),   
-			"La catégorie 1 a deux produits différents vendus dans le jeu de test");
-		assertEquals(35l, results.get(1).get("unites"), "On a vendu 35 unités du produit 'Chang' dans le jeu de test");
-	}		
 }
