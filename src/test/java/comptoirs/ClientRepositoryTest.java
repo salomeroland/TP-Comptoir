e15cb5ca075c052a54a9b78bf566a462973c1cfe
Rémi Bastide <Remi.Bastide@gmail.com>
2020-05-28 17:50:41 +0200

    New test

--- src/test/java/comptoirs/ClientRepositoryTest.java
+++ src/test/java/comptoirs/ClientRepositoryTest.java
@@ -12,10 +12,6 @@ import org.springframework.test.context.jdbc.Sql;
 
 import comptoirs.dao.*;
 import comptoirs.entity.*;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import org.springframework.dao.DataIntegrityViolationException;
 
 @DataJpaTest
 class ClientRepositoryTest {
@@ -34,25 +30,24 @@ class ClientRepositoryTest {
 	@Sql("small_data.sql")		
 	void onPeutTrouverUnClientEtSesCommandes() {
 		logger.debug("Recherche d'un client");
-		// On cherche le client d'après sa clé
-		Client bonap  = daoClient.getOne("BONAP");
+		// On cherche le client BONAP d'après sa clé
+
 		// On vérifie qu'il a des commandes
-		assertEquals(2, bonap.getCommandeList().size(), "Bonap à deux commandes dans le jeu de tests");
 	}
 	
 	@Test
 	@Sql("small_data.sql")		
 	void supprimerUnClientSupprimeAussiSesCommandes() {
 		logger.debug("On supprime un client");	
-		// Au début, on a deux commandes 
-		assertEquals(2, daoCommande.count(), "Il y a deux commandes dans le jeu de tests");
-		// On cherche le client d'après sa clé
-		Client bonap  = daoClient.getOne("BONAP");
+		// On vérifie qu'au début, on a deux commandes 
+
+		// On cherche le client BONAP d'après sa clé
+
 		// On supprime le client
-		daoClient.delete(bonap);
+
 		// On vérifie qu'il ne reste aucune commande
-		assertEquals(0, daoCommande.count(), "Il ne reste aucune commande après la suppression du client");
+
 		// On vérifie qu'il ne reste aucune ligne
-		assertEquals(0, daoLigne.count(), "Il ne reste aucune ligne de commande après la suppression du client");
+
 	}
 }
